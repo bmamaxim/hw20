44052f96e7fdb677df4de817f5730c657fb7ab26
bmamaxim <bma@gmail.com>
2024-05-14 19:37:48 +0500

    user

--- users/models.py
+++ users/models.py
@@ -1,3 +1,29 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+class User(AbstractUser):
+
+    username = None
+    email = models.EmailField(unique=True, verbose_name='элктронная почта')
+    avatar = models.ImageField(upload_to='image/', verbose_name='изображение', **NULLABLE)
+    phone = models.CharField(max_length=200, verbose_name='телефон', **NULLABLE)
+    country = models.CharField(max_length=200, verbose_name='страна', **NULLABLE)
+    ver_code = models.CharField(max_length=4, verbose_name='код верификации', **NULLABLE)
+
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+
+    class Meta:
+        verbose_name = 'пользователь'
+        verbose_name_plural = 'пользователи'
+
+
+
+    def __str__(self):
+        return f"{self.email} {self.phone} {self.country}"
+
+
