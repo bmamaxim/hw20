714575b6debf300ee378e757f9e5307f357caaeb
bmamaxim <bma@gmail.com>
2024-05-16 15:33:51 +0500

    material

--- materials/urls.py
+++ materials/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from rest_framework.routers import DefaultRouter, SimpleRouter
 
 from materials.apps import MaterialsConfig
-from materials.views import DirectionViewSet, LessonCreateAPIView
+from materials.views import DirectionViewSet, LessonCreateAPIView, LessonListAPIView, LessonRetrieveAPIView, \
+    LessonUpdateAPIView, LessonDestroyAPIView
 
 app_name = MaterialsConfig.name
 
@@ -11,4 +12,8 @@ router.register(r'dir', DirectionViewSet, basename='direction')
 
 urlpatterns = [
     path('lesson/create/', LessonCreateAPIView.as_view(), name='create'),
+    path('lesson/', LessonListAPIView.as_view(), name='lesson'),
+    path('lesson/<int:pk>/', LessonRetrieveAPIView.as_view(), name='view'),
+    path('lesson/update/<int:pk>/', LessonUpdateAPIView.as_view(), name='update'),
+    path('lesson/delete/<int:pk>/', LessonDestroyAPIView.as_view(), name='delete'),
 ] + router.urls
--- materials/views.py
+++ materials/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from rest_framework import viewsets, generics
 
 from materials.models import Direction, Lesson
@@ -8,8 +7,27 @@ from materials.serializers import DirectionSerializer, LessonSerializer
 class DirectionViewSet(viewsets.ModelViewSet):
 
     serializer_class = DirectionSerializer
-    queryset = Direction.objects.all
+    queryset = Direction.objects.all()
 
 
 class LessonCreateAPIView(generics.CreateAPIView):
     serializer_class = LessonSerializer
+
+
+class LessonListAPIView(generics.ListAPIView):
+    serializer_class = LessonSerializer
+    queryset = Lesson.objects.all()
+
+
+class LessonRetrieveAPIView(generics.RetrieveAPIView):
+    serializer_class = LessonSerializer
+    queryset = Lesson.objects.all()
+
+
+class LessonUpdateAPIView(generics.UpdateAPIView):
+    serializer_class = LessonSerializer
+    queryset = Lesson.objects.all()
+
+
+class LessonDestroyAPIView(generics.DestroyAPIView):
+    queryset = Lesson.objects.all()
