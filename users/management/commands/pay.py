6543ada0ed29e02b68ac8495d481280f01af8f01
bmamaxim <bma@gmail.com>
2024-05-18 16:39:45 +0500

    lesson

--- users/management/commands/pay.py
+++ users/management/commands/pay.py
@@ -2,7 +2,8 @@ import json
 
 from django.core.management import BaseCommand
 
-from users.models import Payment
+from materials.models import Direction, Lesson
+from users.models import Payment, User
 
 
 class Command(BaseCommand):
@@ -15,7 +16,13 @@ class Command(BaseCommand):
             pay_for_create = []
 
             for model in data:
-                if model['model'] == 'user.payment':
-                    pay_for_create.append(model)
+                if model['model'] == "users.payment":
+                    pay_for_create.append(Payment(user=User.objects.get(pk=model['fields']['user']),
+                                                  direction=Direction.objects.get(pk=model['fields']['direction']),
+                                                  lesson=Lesson.objects.get(pk=model['fields']['direction']),
+                                                  payment_sign=model['fields']['payment_sign'],
+                                                  payment_sum=model['fields']['payment_sum'],
+                                                  payment_method=model['fields']['payment_method'],
+                                                  payment_date=model['fields']['payment_date']))
 
             Payment.objects.bulk_create(pay_for_create)
